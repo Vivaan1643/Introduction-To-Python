587fd132c00441f399db879cf479419389c15d47
Vivaan1643 <vivaan.k.urs@gmail.com>
2024-11-12 17:04:10 +0530

    first commit

---
+++ Intro To Python/main.py
@@ -0,0 +1,17 @@
+#Printing a Greet Message
+print("Hello World!")
+#Printing my name
+print("My Name is Vivaan")
+#Printing a fun fact about myself
+print("I love playing cricket")
+#Performing and printing a sample calculation
+print("The result of 10 + 20 is:", 10+20)
+
+
+
+print("Welcome to the world of programming")
+print(10)
+print ("Hello Python \n")
+print ("Keywords in Python are ...")
+import keyword
+print(keyword.kwlist)
